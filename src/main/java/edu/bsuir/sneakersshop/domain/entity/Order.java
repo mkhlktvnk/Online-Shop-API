d28915ce6b282d2d51c9cb5f9ae2c0627fdd57f1
misha <mikhail.kutovenko@gmail.com>
2023-03-10 18:48:08 +0300

    Add Order

---
+++ src/main/java/edu/bsuir/sneakersshop/domain/entity/Order.java
@@ -0,0 +1,43 @@
+package edu.bsuir.sneakersshop.domain.entity;
+
+import edu.bsuir.sneakersshop.domain.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "orders")
+@Data
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "order")
+    private Set<OrderItem> orderItems = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @PrePersist
+    private void calculateTotalPrice() {
+        orderItems.forEach(orderItem -> totalPrice = totalPrice.add(orderItem.getTotalPrice()));
+    }
+}

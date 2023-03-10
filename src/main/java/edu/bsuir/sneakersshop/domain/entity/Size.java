3a3d6fd107b7be3c91da6b7573da87f69c6adc5a
misha <mikhail.kutovenko@gmail.com>
2023-03-10 14:44:44 +0300

    Add Size

---
+++ src/main/java/edu/bsuir/sneakersshop/domain/entity/Size.java
@@ -0,0 +1,21 @@
+package edu.bsuir.sneakersshop.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "sizes")
+public class Size {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer sizeValue;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+}

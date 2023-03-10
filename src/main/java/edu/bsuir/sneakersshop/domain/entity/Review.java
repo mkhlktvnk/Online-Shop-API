181b3670c6c819225b0f4db5791d93357b03585e
misha <mikhail.kutovenko@gmail.com>
2023-03-10 19:07:53 +0300

    Add Review

---
+++ src/main/java/edu/bsuir/sneakersshop/domain/entity/Review.java
@@ -0,0 +1,31 @@
+package edu.bsuir.sneakersshop.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String topic;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Integer mark;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+}

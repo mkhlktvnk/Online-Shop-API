65e66f7bda37fd2b974b1b34ec355f74aea748a6
misha <mikhail.kutovenko@gmail.com>
2023-03-10 14:44:51 +0300

    Add Image

---
+++ src/main/java/edu/bsuir/sneakersshop/domain/entity/Image.java
@@ -0,0 +1,20 @@
+package edu.bsuir.sneakersshop.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "product_images")
+@Data
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Product product;
+}

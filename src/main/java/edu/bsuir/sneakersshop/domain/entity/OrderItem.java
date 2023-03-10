0421e6a5ff148639f5dd231a5381c945b4a56faf
misha <mikhail.kutovenko@gmail.com>
2023-03-10 18:35:29 +0300

    Add OrderItem

---
+++ src/main/java/edu/bsuir/sneakersshop/domain/entity/OrderItem.java
@@ -0,0 +1,36 @@
+package edu.bsuir.sneakersshop.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "order_items")
+@Data
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Integer productSize;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer productQuantity;
+
+    @Column(nullable = false)
+    private BigDecimal totalPrice;
+
+    @PrePersist
+    void calculateTotalPrice() {
+        totalPrice = BigDecimal.valueOf(productQuantity, 2).multiply(price);
+    }
+}

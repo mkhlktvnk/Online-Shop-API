5ee0e18b4f5cecf7025e162073a8f4329456bfd4
misha <mikhail.kutovenko@gmail.com>
2023-03-10 17:46:36 +0300

    Update User: add phone number

--- src/main/java/edu/bsuir/sneakersshop/domain/entity/User.java
+++ src/main/java/edu/bsuir/sneakersshop/domain/entity/User.java
@@ -20,6 +20,9 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
     @Column(nullable = false)
     private String password;
 

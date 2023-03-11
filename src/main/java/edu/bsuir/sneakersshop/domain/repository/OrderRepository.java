c5dd8de5a97acd0748cdfaebc6a2b0d3015c0518
misha <mikhail.kutovenko@gmail.com>
2023-03-11 09:29:22 +0300

    Update OrderRepository: add Pageable argument

--- src/main/java/edu/bsuir/sneakersshop/domain/repository/OrderRepository.java
+++ src/main/java/edu/bsuir/sneakersshop/domain/repository/OrderRepository.java
@@ -1,6 +1,7 @@
 package edu.bsuir.sneakersshop.domain.repository;
 
 import edu.bsuir.sneakersshop.domain.entity.Order;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
-    List<Order> findAllByUserId(Long userId);
+    List<Order> findAllByUserId(Long userId, Pageable pageable);
 }

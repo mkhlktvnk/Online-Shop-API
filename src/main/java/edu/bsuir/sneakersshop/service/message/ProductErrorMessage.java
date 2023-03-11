44f4832892b9fac3fbb82c02eb4a6f48f5a565f0
misha <mikhail.kutovenko@gmail.com>
2023-03-11 11:21:32 +0300

    Add ProductErrorMessage

---
+++ src/main/java/edu/bsuir/sneakersshop/service/message/ProductErrorMessage.java
@@ -0,0 +1,17 @@
+package edu.bsuir.sneakersshop.service.message;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@PropertySource("classpath:error-message.properties")
+public class ProductErrorMessage {
+    @Value("classpath:product.not-found.message")
+    private String notFoundMessage;
+}
+
+
+

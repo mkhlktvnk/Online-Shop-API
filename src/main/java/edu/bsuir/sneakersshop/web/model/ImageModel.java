2f198ee2215b9e0d0124103bcac5873f24e8fa7a
misha <mikhail.kutovenko@gmail.com>
2023-03-12 18:09:07 +0300

    Update ImageModel: add validation constraints

--- src/main/java/edu/bsuir/sneakersshop/web/model/ImageModel.java
+++ src/main/java/edu/bsuir/sneakersshop/web/model/ImageModel.java
@@ -1,6 +1,9 @@
 package edu.bsuir.sneakersshop.web.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +15,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ImageModel {
     @JsonProperty("id")
+    @NotNull
+    @Min(0)
     private Long id;
 
     @JsonProperty("imageUrl")
+    @NotNull
+    @NotBlank
     private String imageUrl;
 }

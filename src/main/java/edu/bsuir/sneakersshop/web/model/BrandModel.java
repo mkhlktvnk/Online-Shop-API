dd6871e7c25c449364982ec6e87f4f035cf104ce
misha <mikhail.kutovenko@gmail.com>
2023-03-12 18:08:29 +0300

    Update BrandModel: add validation constraints

--- src/main/java/edu/bsuir/sneakersshop/web/model/BrandModel.java
+++ src/main/java/edu/bsuir/sneakersshop/web/model/BrandModel.java
@@ -1,10 +1,14 @@
 package edu.bsuir.sneakersshop.web.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Builder
 @Data
@@ -12,11 +16,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class BrandModel {
     @JsonProperty("id")
+    @Min(1)
+    @NotNull
     private Long id;
 
     @JsonProperty("name")
+    @NotNull
+    @NotBlank
+    @Length(max = 255)
     private String name;
 
     @JsonProperty("description")
+    @NotNull
+    @NotBlank
+    @Length(max = 5000)
     private String description;
 }

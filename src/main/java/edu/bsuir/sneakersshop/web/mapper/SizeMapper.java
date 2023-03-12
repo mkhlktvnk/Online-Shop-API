fce131944ec12f0b3dc2d9cf8ae0e3aac10d97bb
misha <mikhail.kutovenko@gmail.com>
2023-03-12 22:42:19 +0300

    Update SizeMapper: add methods for mapping collections

--- src/main/java/edu/bsuir/sneakersshop/web/mapper/SizeMapper.java
+++ src/main/java/edu/bsuir/sneakersshop/web/mapper/SizeMapper.java
@@ -4,9 +4,15 @@ import edu.bsuir.sneakersshop.domain.entity.Size;
 import edu.bsuir.sneakersshop.web.model.SizeModel;
 import org.mapstruct.Mapper;
 
+import java.util.Collection;
+
 @Mapper
 public interface SizeMapper {
     Size mapToEntity(SizeModel sizeModel);
 
     SizeModel mapToModel(Size size);
+
+    Collection<Size> mapToEntity(Collection<SizeModel> sizeModels);
+
+    Collection<SizeModel> mapToModel(Collection<Size> sizes);
 }

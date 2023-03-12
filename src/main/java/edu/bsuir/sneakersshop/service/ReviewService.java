533c380e8228b9073c842d4b0088cd8b63937916
misha <mikhail.kutovenko@gmail.com>
2023-03-12 23:14:51 +0300

    Add ReviewService

---
+++ src/main/java/edu/bsuir/sneakersshop/service/ReviewService.java
@@ -0,0 +1,20 @@
+package edu.bsuir.sneakersshop.service;
+
+import edu.bsuir.sneakersshop.domain.entity.Review;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ReviewService {
+    Review findOne(Long id);
+
+    List<Review> findAll(Pageable pageable);
+
+    List<Review> findByUserId(Long userId, Pageable pageable);
+
+    Review insert(Review review);
+
+    void update(Long id, Review review);
+
+    void delete(Long id);
+}

6eaed210784a97cc58f5997746d3d72bfe983a02
misha <mikhail.kutovenko@gmail.com>
2023-03-12 23:17:42 +0300

    Add ReviewErrorMessage

---
+++ src/main/java/edu/bsuir/sneakersshop/service/message/ReviewErrorMessage.java
@@ -0,0 +1,14 @@
+package edu.bsuir.sneakersshop.service.message;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@PropertySource("classpath:error-message.properties")
+public class ReviewErrorMessage {
+    @Value("classpath:review.not-found.message")
+    private String notFound;
+}

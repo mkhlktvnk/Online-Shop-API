4dda4e560b5727d9676969c645e9e3cdcdd6a2f3
misha <mikhail.kutovenko@gmail.com>
2023-03-12 18:13:10 +0300

    Update SizeModel: add validation constraints

--- src/main/java/edu/bsuir/sneakersshop/web/model/SizeModel.java
+++ src/main/java/edu/bsuir/sneakersshop/web/model/SizeModel.java
@@ -1,6 +1,8 @@
 package edu.bsuir.sneakersshop.web.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +14,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SizeModel {
     @JsonProperty("id")
+    @NotNull
+    @Min(0)
     private Long id;
 
     @JsonProperty("sizeValue")
+    @NotNull
+    @Min(1)
     private Integer sizeValue;
 
     @JsonProperty("quantity")
+    @NotNull
+    @Min(0)
     private Integer quantity;
 }

183d1f27e99999f126b14ad2e3f4102ac454306c
misha <mikhail.kutovenko@gmail.com>
2023-03-12 23:20:39 +0300

    Update ReviewRepository: add inheritance from PagingAndSortingRepository

--- src/main/java/edu/bsuir/sneakersshop/domain/repository/ReviewRepository.java
+++ src/main/java/edu/bsuir/sneakersshop/domain/repository/ReviewRepository.java
@@ -3,11 +3,12 @@ package edu.bsuir.sneakersshop.domain.repository;
 import edu.bsuir.sneakersshop.domain.entity.Review;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ReviewRepository extends CrudRepository<Review, Long> {
+public interface ReviewRepository extends CrudRepository<Review, Long>, PagingAndSortingRepository<Review, Long> {
     List<Review> findAllByUserId(Long id, Pageable pageable);
 }

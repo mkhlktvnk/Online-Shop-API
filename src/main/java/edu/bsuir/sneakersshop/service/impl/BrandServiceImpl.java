69b570615731528d3e12fd25020f599e3153ee04
misha <mikhail.kutovenko@gmail.com>
2023-03-12 16:08:01 +0300

    Update BrandServiceImpl: implement methods

--- src/main/java/edu/bsuir/sneakersshop/service/impl/BrandServiceImpl.java
+++ src/main/java/edu/bsuir/sneakersshop/service/impl/BrandServiceImpl.java
@@ -3,6 +3,8 @@ package edu.bsuir.sneakersshop.service.impl;
 import edu.bsuir.sneakersshop.domain.entity.Brand;
 import edu.bsuir.sneakersshop.domain.repository.BrandRepository;
 import edu.bsuir.sneakersshop.service.BrandService;
+import edu.bsuir.sneakersshop.service.exception.EntityNotFoundException;
+import edu.bsuir.sneakersshop.service.message.BrandErrorMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BrandServiceImpl implements BrandService {
     private final BrandRepository brandRepository;
+    private final BrandErrorMessage brandErrorMessage;
 
     @Override
     public Brand insert(Brand brand) {
@@ -21,22 +24,31 @@ public class BrandServiceImpl implements BrandService {
 
     @Override
     public void update(Long id, Brand brand) {
-
+        Brand brandToUpdate = brandRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(brandErrorMessage.getNotFoundMessage()));
+        brandToUpdate.setName(brand.getName());
+        brandToUpdate.setDescription(brand.getDescription());
+        brandRepository.save(brandToUpdate);
     }
 
     @Override
     public void delete(Long id) {
-
+        if (!brandRepository.existsById(id)) {
+            throw new EntityNotFoundException(brandErrorMessage.getNotFoundMessage());
+        }
+        brandRepository.deleteById(id);
     }
 
     @Override
     public Brand findOne(Long id) {
-        return null;
+        return brandRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(brandErrorMessage.getNotFoundMessage()));
     }
 
     @Override
     public Brand findOneByName(String name) {
-        return null;
+        return brandRepository.findByName(brandErrorMessage.getNotFoundMessage())
+                .orElseThrow(() -> new EntityNotFoundException(brandErrorMessage.getNotFoundMessage()));
     }
 
     @Override

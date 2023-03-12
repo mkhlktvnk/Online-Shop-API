26b6f4c97125117d6f080e8c2c34d7abb7b98349
misha <mikhail.kutovenko@gmail.com>
2023-03-12 23:11:12 +0300

    Update ReviewModel: add annotations

--- src/main/java/edu/bsuir/sneakersshop/web/model/ReviewModel.java
+++ src/main/java/edu/bsuir/sneakersshop/web/model/ReviewModel.java
@@ -5,8 +5,16 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewModel {
     @JsonProperty("id")
     @NotNull

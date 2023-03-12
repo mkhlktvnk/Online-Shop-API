543b0be66c80b754831a4259f5eccd45ce95624c
misha <mikhail.kutovenko@gmail.com>
2023-03-12 22:41:28 +0300

    Update ProductMapper: add methods for mapping collections

--- src/main/java/edu/bsuir/sneakersshop/web/mapper/ProductMapper.java
+++ src/main/java/edu/bsuir/sneakersshop/web/mapper/ProductMapper.java
@@ -4,9 +4,15 @@ import edu.bsuir.sneakersshop.domain.entity.Product;
 import edu.bsuir.sneakersshop.web.model.ProductModel;
 import org.mapstruct.Mapper;
 
+import java.util.Collection;
+
 @Mapper
 public interface ProductMapper {
     Product mapToEntity(ProductModel productModel);
 
     ProductModel mapToModel(Product product);
+
+    Collection<Product> mapToEntity(Collection<ProductModel> productModels);
+
+    Collection<ProductModel> mapToModel(Collection<Product> products);
 }

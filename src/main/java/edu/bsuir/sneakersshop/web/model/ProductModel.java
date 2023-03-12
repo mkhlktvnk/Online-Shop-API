ce9be2d09d73ba7506b5b939dee8940c63018e2f
misha <mikhail.kutovenko@gmail.com>
2023-03-12 18:12:19 +0300

    Update ProductModel: add validation constraints

--- src/main/java/edu/bsuir/sneakersshop/web/model/ProductModel.java
+++ src/main/java/edu/bsuir/sneakersshop/web/model/ProductModel.java
@@ -2,10 +2,12 @@ package edu.bsuir.sneakersshop.web.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import edu.bsuir.sneakersshop.domain.enums.SeasonType;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import java.math.BigDecimal;
 import java.util.Set;
@@ -16,18 +18,31 @@ import java.util.Set;
 @AllArgsConstructor
 public class ProductModel {
     @JsonProperty("id")
+    @NotNull
+    @Min(0)
     private Long id;
 
     @JsonProperty("name")
+    @NotNull
+    @NotBlank
+    @Length(max = 255)
     private String name;
 
     @JsonProperty("description")
+    @NotNull
+    @NotBlank
+    @Length(max = 255)
     private String description;
 
     @JsonProperty("price")
+    @NotNull
+    @DecimalMin("0.00")
+    @Digits(integer = 50, fraction = 2)
     private BigDecimal price;
 
     @JsonProperty("season")
+    @NotNull
+    @NotBlank
     private SeasonType seasonType;
 
     @JsonProperty("images")

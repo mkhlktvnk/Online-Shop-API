507d8ae7c15384bc1c2064842952b4b32aef3cbe
misha <mikhail.kutovenko@gmail.com>
2023-03-12 17:48:30 +0300

    Update ImageModel

--- src/main/java/edu/bsuir/sneakersshop/web/model/ImageModel.java
+++ src/main/java/edu/bsuir/sneakersshop/web/model/ImageModel.java
@@ -1,8 +1,15 @@
 package edu.bsuir.sneakersshop.web.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImageModel {
+    private Long id;
     private String imageUrl;
 }

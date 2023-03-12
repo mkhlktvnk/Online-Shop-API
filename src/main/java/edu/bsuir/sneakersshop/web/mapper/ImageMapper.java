6c193f6714237b22d3ebf8de969066c506d42363
misha <mikhail.kutovenko@gmail.com>
2023-03-12 22:40:06 +0300

    Update ImageMapper: add methods for mapping collections

--- src/main/java/edu/bsuir/sneakersshop/web/mapper/ImageMapper.java
+++ src/main/java/edu/bsuir/sneakersshop/web/mapper/ImageMapper.java
@@ -4,8 +4,12 @@ import edu.bsuir.sneakersshop.domain.entity.Image;
 import edu.bsuir.sneakersshop.web.model.ImageModel;
 import org.mapstruct.Mapper;
 
+import java.util.Collection;
+
 @Mapper
 public interface ImageMapper {
     Image mapToEntity(ImageModel imageModel);
     ImageModel mapToModel(Image image);
+    Collection<Image> mapToEntity(Collection<ImageModel> imageModels);
+    Collection<ImageModel> mapToModel(Collection<Image> images);
 }

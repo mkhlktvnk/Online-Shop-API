581f20c6137cab6f0b5bc55967e6b0110f3a5b19
misha <mikhail.kutovenko@gmail.com>
2023-03-13 21:24:29 +0300

    Update UserRepository: add methods to check user existence by username, email and phoneNumber

--- src/main/java/edu/bsuir/sneakersshop/domain/repository/UserRepository.java
+++ src/main/java/edu/bsuir/sneakersshop/domain/repository/UserRepository.java
@@ -9,4 +9,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
 }

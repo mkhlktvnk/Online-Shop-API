2b7f3ac016a44abb4a67f74315232329c39efc28
misha <mikhail.kutovenko@gmail.com>
2023-03-13 21:20:35 +0300

    Update UserService: change param of delete method from username to id

--- src/main/java/edu/bsuir/sneakersshop/service/UserService.java
+++ src/main/java/edu/bsuir/sneakersshop/service/UserService.java
@@ -5,5 +5,5 @@ import edu.bsuir.sneakersshop.domain.entity.User;
 public interface UserService {
     User findByUsername(String username);
     User insert(User user);
-    void delete(String username);
+    void delete(Long id);
 }

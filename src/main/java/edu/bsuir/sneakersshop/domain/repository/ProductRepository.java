31062914528805936065c60476ddf345f836ea43
misha <mikhail.kutovenko@gmail.com>
2023-03-27 16:11:43 +0300

    Update ProductRepository: extend JpaSpecificationRepository

--- src/main/java/edu/bsuir/sneakersshop/domain/repository/ProductRepository.java
+++ src/main/java/edu/bsuir/sneakersshop/domain/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package edu.bsuir.sneakersshop.domain.repository;
 import edu.bsuir.sneakersshop.domain.entity.Product;
 import edu.bsuir.sneakersshop.domain.enums.SeasonType;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,8 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long>, PagingAndSortingRepository<Product, Long> {
+public interface ProductRepository extends CrudRepository<Product, Long>,
+        PagingAndSortingRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     List<Product> findAllBySeasonType(SeasonType seasonType, Pageable pageable);
     List<Product> findAllByPriceBetween(BigDecimal min, BigDecimal max, Pageable pageable);
     List<Product> findAllByBrandId(Long brandId, Pageable pageable);

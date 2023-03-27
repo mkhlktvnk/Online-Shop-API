275e40d2f638b9039927518756b88e18686941db
misha <mikhail.kutovenko@gmail.com>
2023-03-27 15:53:39 +0300

    Add ProductCriteria

---
+++ src/main/java/edu/bsuir/sneakersshop/web/ProductCriteria.java
@@ -0,0 +1,18 @@
+package edu.bsuir.sneakersshop.web;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProductCriteria {
+    private String name;
+    private String description;
+    private BigDecimal minPrice;
+    private BigDecimal maxPrice;
+    private String brandName;
+}

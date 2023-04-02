929bcb1b7c6ff9296f4cc1ec35a7180fe75efe18
misha <mikhail.kutovenko@gmail.com>
2023-04-02 18:04:12 +0300

    Add constraints to ProductCriteria

--- src/main/java/edu/bsuir/sneakersshop/web/criteria/ProductCriteria.java
+++ src/main/java/edu/bsuir/sneakersshop/web/criteria/ProductCriteria.java
@@ -1,5 +1,9 @@
 package edu.bsuir.sneakersshop.web.criteria;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,9 +14,24 @@ import java.math.BigDecimal;
 @Setter
 @NoArgsConstructor
 public class ProductCriteria {
+
+    @NotBlank
+    @Size(min = 1)
     private String name;
+
+    @NotBlank
+    @Size(min = 1)
     private String description;
+
+    @Min(1)
+    @Digits(integer = 40, fraction = 2)
     private BigDecimal minPrice;
+
+    @Min(1)
+    @Digits(integer = 40, fraction = 2)
     private BigDecimal maxPrice;
+
+    @NotBlank
+    @Size(min = 1)
     private String brandName;
 }

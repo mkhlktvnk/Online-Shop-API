9ef0ed6d4e727b930d631df8e10602095157eb52
misha <mikhail.kutovenko@gmail.com>
2023-04-10 01:44:14 +0300

    Add OrderServiceImpl

---
+++ src/main/java/edu/bsuir/sneakersshop/service/impl/OrderServiceImpl.java
@@ -0,0 +1,41 @@
+package edu.bsuir.sneakersshop.service.impl;
+
+import edu.bsuir.sneakersshop.domain.entity.Order;
+import edu.bsuir.sneakersshop.domain.entity.OrderItem;
+import edu.bsuir.sneakersshop.domain.entity.Product;
+import edu.bsuir.sneakersshop.domain.entity.User;
+import edu.bsuir.sneakersshop.domain.repository.OrderRepository;
+import edu.bsuir.sneakersshop.service.OrderService;
+import edu.bsuir.sneakersshop.service.ProductService;
+import edu.bsuir.sneakersshop.service.UserService;
+import edu.bsuir.sneakersshop.web.request.OrderRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+    private final ProductService productService;
+    private final UserService userService;
+
+    @Override
+    @Transactional
+    public Order makeOrder(OrderRequest orderRequest) {
+        User user = userService.findById(orderRequest.getUserId());
+        Product product = productService.findById(orderRequest.getProductId());
+
+        OrderItem orderItem = new OrderItem();
+        orderItem.setProduct(product);
+        orderItem.setProductSize(orderRequest.getProductSize());
+        orderItem.setProductQuantity(orderRequest.getProductQuantity());
+
+        Order order = new Order();
+        order.setOrderItem(orderItem);
+        order.setUser(user);
+
+        return orderRepository.save(order);
+    }
+
+}

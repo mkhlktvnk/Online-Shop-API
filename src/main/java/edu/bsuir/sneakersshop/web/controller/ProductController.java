5a57d13efb469d01aded24b8742cecf3193c4f36
misha <mikhail.kutovenko@gmail.com>
2023-04-10 00:29:46 +0300

    Update controllers: add @RequestMapping with '/api/v0'

--- src/main/java/edu/bsuir/sneakersshop/web/controller/ProductController.java
+++ src/main/java/edu/bsuir/sneakersshop/web/controller/ProductController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v0")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
--- src/main/java/edu/bsuir/sneakersshop/web/controller/ReviewController.java
+++ src/main/java/edu/bsuir/sneakersshop/web/controller/ReviewController.java
@@ -9,11 +9,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/v0")
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;

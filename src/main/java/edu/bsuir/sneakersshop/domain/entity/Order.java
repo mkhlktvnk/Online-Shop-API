82b89ed8b56c75e27be2f11028a7c97865c4efaf
misha <mikhail.kutovenko@gmail.com>
2023-04-10 01:45:01 +0300

    Update Order

--- src/main/java/edu/bsuir/sneakersshop/domain/entity/Order.java
+++ src/main/java/edu/bsuir/sneakersshop/domain/entity/Order.java
@@ -1,19 +1,6 @@
 package edu.bsuir.sneakersshop.domain.entity;
 
-import edu.bsuir.sneakersshop.domain.entity.enums.OrderStatus;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,8 +9,6 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "orders")
@@ -45,19 +30,15 @@ public class Order {
     @CreationTimestamp
     private Timestamp createdAt;
 
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private OrderStatus orderStatus;
-
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "order")
-    private Set<OrderItem> orderItems = new HashSet<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_item_id", nullable = false)
+    private OrderItem orderItem;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
     @PrePersist
-    private void calculateTotalPrice() {
-        orderItems.forEach(orderItem -> totalPrice = totalPrice.add(orderItem.getTotalPrice()));
+    void calculateTotalPrice() {
+        totalPrice = orderItem.getTotalPrice();
     }
-
 }

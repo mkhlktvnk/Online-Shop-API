3dee2ba1e8724b7549dc9ad38325c5a5087f22d8
misha <mikhail.kutovenko@gmail.com>
2023-04-10 05:45:41 +0300

    Add UserModel

---
+++ src/main/java/edu/bsuir/sneakersshop/web/model/UserModel.java
@@ -0,0 +1,45 @@
+package edu.bsuir.sneakersshop.web.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModel {
+
+    @NotNull
+    @JsonProperty(value =  "id", access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 1)
+    @JsonProperty(value = "username")
+    private String username;
+
+    @NotNull
+    @Email
+    @JsonProperty(value = "email")
+    private String email;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 8)
+    @JsonProperty(value = "password")
+    private String password;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 1)
+    @JsonProperty(value = "postIndex")
+    private String postIndex;
+}

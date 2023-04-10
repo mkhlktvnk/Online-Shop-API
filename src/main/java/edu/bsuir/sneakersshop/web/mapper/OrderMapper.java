dae7c691431597f5581a85e6b9732a765bfd6a46
misha <mikhail.kutovenko@gmail.com>
2023-04-10 11:52:23 +0300

    Update OrderMapper

--- src/main/java/edu/bsuir/sneakersshop/web/mapper/OrderMapper.java
+++ src/main/java/edu/bsuir/sneakersshop/web/mapper/OrderMapper.java
@@ -3,8 +3,10 @@ package edu.bsuir.sneakersshop.web.mapper;
 import edu.bsuir.sneakersshop.domain.entity.Order;
 import edu.bsuir.sneakersshop.web.model.OrderModel;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(uses = OrderItemMapper.class)
+@Mapper(uses = ProductMapper.class)
 public interface OrderMapper {
+    @Mapping(target = "productModel", source = "product")
     OrderModel mapToModel(Order order);
 }

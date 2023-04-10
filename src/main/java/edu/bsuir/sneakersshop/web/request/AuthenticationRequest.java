a8a8a01a7fd9300bac919f7e461443e604900c7e
misha <mikhail.kutovenko@gmail.com>
2023-04-10 06:35:02 +0300

    Add AuthenticationRequest

---
+++ src/main/java/edu/bsuir/sneakersshop/web/request/AuthenticationRequest.java
@@ -0,0 +1,28 @@
+package edu.bsuir.sneakersshop.web.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AuthenticationRequest {
+
+    @NotNull
+    @NotBlank
+    @Size(min = 1)
+    @JsonProperty(value = "username", access = JsonProperty.Access.WRITE_ONLY)
+    private String username;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 1)
+    @JsonProperty(value = "username", access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+}

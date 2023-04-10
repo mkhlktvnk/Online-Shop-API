62ae48921915e893d169b5ce4b7897ecdbc48247
misha <mikhail.kutovenko@gmail.com>
2023-04-10 13:08:54 +0300

    Update OrderService

--- src/main/java/edu/bsuir/sneakersshop/service/OrderService.java
+++ src/main/java/edu/bsuir/sneakersshop/service/OrderService.java
@@ -9,6 +9,10 @@ import java.util.List;
 public interface OrderService {
     List<Order> findAll(Pageable pageable);
 
+    Order findById(long id);
+
+    Order findByUserAndOrderId(long userId, long orderId);
+
     List<Order> findAllByUserId(long userId, Pageable pageable);
 
     Order makeOrder(long userId, OrderRequest orderRequest);

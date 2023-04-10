b1ecfd67ca3efa41dfae8d03e064457732b54ff3
misha <mikhail.kutovenko@gmail.com>
2023-04-10 05:34:41 +0300

    Update UserService

--- src/main/java/edu/bsuir/sneakersshop/service/UserService.java
+++ src/main/java/edu/bsuir/sneakersshop/service/UserService.java
@@ -1,7 +1,9 @@
 package edu.bsuir.sneakersshop.service;
 
 import edu.bsuir.sneakersshop.domain.entity.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     User findById(long id);
+    User insert(User user);
 }
--- src/main/java/edu/bsuir/sneakersshop/service/impl/UserServiceImpl.java
+++ src/main/java/edu/bsuir/sneakersshop/service/impl/UserServiceImpl.java
@@ -1,20 +1,62 @@
 package edu.bsuir.sneakersshop.service.impl;
 
+import edu.bsuir.sneakersshop.domain.entity.Role;
 import edu.bsuir.sneakersshop.domain.entity.User;
+import edu.bsuir.sneakersshop.domain.entity.enums.RoleType;
+import edu.bsuir.sneakersshop.domain.repository.RoleRepository;
 import edu.bsuir.sneakersshop.domain.repository.UserRepository;
 import edu.bsuir.sneakersshop.service.UserService;
+import edu.bsuir.sneakersshop.service.exception.EntityAlreadyExistsException;
 import edu.bsuir.sneakersshop.service.exception.EntityNotFoundException;
+import edu.bsuir.sneakersshop.service.message.UserMessages;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final UserMessages userMessages;
 
     @Override
     public User findById(long id) {
         return userRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Error! User not found"));
+                .orElseThrow(() -> new EntityNotFoundException(userMessages.getNotFound()));
+    }
+
+    @Override
+    @Transactional
+    public User insert(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new EntityAlreadyExistsException(userMessages.getAlreadyExists());
+        }
+        if (userRepository.existsByUsername(user.getUsername())) {
+            throw new EntityAlreadyExistsException(userMessages.getAlreadyExists());
+        }
+        if (userRepository.existsByPhoneNumber(user.getPhoneNumber())) {
+            throw new EntityAlreadyExistsException(userMessages.getAlreadyExists());
+        }
+
+        Role role = roleRepository.findByAuthority(RoleType.USER.getRoleName())
+                .orElse(new Role(RoleType.USER));
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setAuthorities(Collections.singleton(role));
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new EntityNotFoundException(userMessages.getNotFound()));
     }
 }

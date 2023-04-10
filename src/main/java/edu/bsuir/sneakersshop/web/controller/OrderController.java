b425b0ca3c2e1ed1d140867f9e74805e7b13f9dc
misha <mikhail.kutovenko@gmail.com>
2023-04-10 10:23:55 +0300

    Update OrderController

--- src/main/java/edu/bsuir/sneakersshop/web/controller/OrderController.java
+++ src/main/java/edu/bsuir/sneakersshop/web/controller/OrderController.java
@@ -1,14 +1,17 @@
 package edu.bsuir.sneakersshop.web.controller;
 
 import edu.bsuir.sneakersshop.domain.entity.Order;
+import edu.bsuir.sneakersshop.domain.entity.User;
 import edu.bsuir.sneakersshop.service.OrderService;
 import edu.bsuir.sneakersshop.web.mapper.OrderMapper;
 import edu.bsuir.sneakersshop.web.model.OrderModel;
 import edu.bsuir.sneakersshop.web.payload.request.OrderRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.factory.Mappers;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v0")
+@Slf4j
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
@@ -24,8 +28,9 @@ public class OrderController {
 
     @PostMapping("/orders")
     @ResponseStatus(HttpStatus.CREATED)
-    public OrderModel makeOrder(@Valid @RequestBody OrderRequest orderRequest) {
-        Order order = orderService.makeOrder(orderRequest);
+    public OrderModel makeOrder(@AuthenticationPrincipal User user, @Valid @RequestBody OrderRequest orderRequest) {
+        log.info(user.toString());
+        Order order = orderService.makeOrder(user.getId(), orderRequest);
         return orderMapper.mapToModel(order);
     }
 }

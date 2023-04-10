63ec4ed512f47b3b10b66f7a744513a3327e6ec9
misha <mikhail.kutovenko@gmail.com>
2023-04-10 12:06:08 +0300

    Update OrderController

--- src/main/java/edu/bsuir/sneakersshop/web/controller/OrderController.java
+++ src/main/java/edu/bsuir/sneakersshop/web/controller/OrderController.java
@@ -1,7 +1,6 @@
 package edu.bsuir.sneakersshop.web.controller;
 
 import edu.bsuir.sneakersshop.domain.entity.Order;
-import edu.bsuir.sneakersshop.domain.entity.User;
 import edu.bsuir.sneakersshop.service.OrderService;
 import edu.bsuir.sneakersshop.web.mapper.OrderMapper;
 import edu.bsuir.sneakersshop.web.model.OrderModel;
@@ -10,13 +9,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v0")
@@ -26,10 +25,18 @@ public class OrderController {
     private final OrderService orderService;
     private final OrderMapper orderMapper = Mappers.getMapper(OrderMapper.class);
 
-    @PostMapping("/orders")
+    @GetMapping("/users/{userId}/orders")
+    @PreAuthorize("#userId == authentication.principal.id")
+    public List<OrderModel> findAllByUserId(@PathVariable Long userId, @PageableDefault Pageable pageable) {
+        List<Order> orders = orderService.findAllByUserId(userId, pageable);
+        return orderMapper.mapToModel(orders);
+    }
+
+    @PostMapping("/users/{userId}/orders")
     @ResponseStatus(HttpStatus.CREATED)
-    public OrderModel makeOrder(@AuthenticationPrincipal User user, @Valid @RequestBody OrderRequest orderRequest) {
-        Order order = orderService.makeOrder(user.getId(), orderRequest);
+    @PreAuthorize("#userId == authentication.principal.id")
+    public OrderModel makeOrder(@PathVariable Long userId, @Valid @RequestBody OrderRequest orderRequest) {
+        Order order = orderService.makeOrder(userId, orderRequest);
         return orderMapper.mapToModel(order);
     }
 }

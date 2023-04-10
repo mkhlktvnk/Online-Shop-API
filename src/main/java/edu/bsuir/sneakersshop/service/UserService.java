6e7b6bc62bca37b65f6b0bbe200f1104577c8787
misha <mikhail.kutovenko@gmail.com>
2023-04-10 13:09:12 +0300

    Update UserService

--- src/main/java/edu/bsuir/sneakersshop/service/UserService.java
+++ src/main/java/edu/bsuir/sneakersshop/service/UserService.java
@@ -5,5 +5,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
     User findById(long id);
+
     User insert(User user);
+
+    boolean existsById(long id);
 }

87704fa22a5fa2c150cb550faff1fc02d7ee9f32
misha <mikhail.kutovenko@gmail.com>
2023-04-10 04:51:50 +0300

    Add password encoder bean

--- src/main/java/edu/bsuir/sneakersshop/config/SecurityConfig.java
+++ src/main/java/edu/bsuir/sneakersshop/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +24,10 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder(8);
     }
 }

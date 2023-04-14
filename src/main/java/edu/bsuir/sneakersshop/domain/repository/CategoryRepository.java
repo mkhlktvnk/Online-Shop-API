83278993efe7a9e4fbb1a5490640ed0a52b9c58e
misha <mikhail.kutovenko@gmail.com>
2023-04-14 08:53:34 +0300

    Update CategoryRepository: add method to check existence by name

--- src/main/java/edu/bsuir/sneakersshop/domain/repository/CategoryRepository.java
+++ src/main/java/edu/bsuir/sneakersshop/domain/repository/CategoryRepository.java
@@ -12,4 +12,6 @@ public interface CategoryRepository extends JpaRepository<Category, Long>, JpaSp
 
     Page<Category> findAllByProductsId(Long productId, Pageable pageable);
 
+    boolean existsByName(String name);
+
 }

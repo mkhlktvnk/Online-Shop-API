e996a3a623e7a73dab0bf0a82a2a8950e01d4736
misha <mikhail.kutovenko@gmail.com>
2023-04-14 08:55:37 +0300

    Add CategoryCriteria

---
+++ src/main/java/edu/bsuir/sneakersshop/web/criteria/CategoryCriteria.java
@@ -0,0 +1,19 @@
+package edu.bsuir.sneakersshop.web.criteria;
+
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryCriteria {
+
+    @Size(min = 1)
+    private String name;
+
+    @Size(min = 1)
+    private String description;
+
+}

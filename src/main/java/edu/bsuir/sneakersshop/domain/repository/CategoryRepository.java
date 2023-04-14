65d295f94e2c87f16cfa7dd95f89b008a7ac833e
misha <mikhail.kutovenko@gmail.com>
2023-04-14 08:41:57 +0300

    Update CategoryRepository: add method to find by product id

--- src/main/java/edu/bsuir/sneakersshop/domain/repository/CategoryRepository.java
+++ src/main/java/edu/bsuir/sneakersshop/domain/repository/CategoryRepository.java
@@ -1,9 +1,14 @@
 package edu.bsuir.sneakersshop.domain.repository;
 
 import edu.bsuir.sneakersshop.domain.entity.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Page<Category> findAllByProductsId(Long productId, Pageable pageable);
+
 }

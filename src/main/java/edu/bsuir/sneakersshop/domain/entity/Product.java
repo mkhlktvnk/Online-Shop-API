2a87233e993a3fba51a13b4e8b78e6affc87bc77
misha <mikhail.kutovenko@gmail.com>
2023-04-14 08:22:30 +0300

    Add Category

--- src/main/java/edu/bsuir/sneakersshop/domain/entity/Product.java
+++ src/main/java/edu/bsuir/sneakersshop/domain/entity/Product.java
@@ -1,18 +1,7 @@
 package edu.bsuir.sneakersshop.domain.entity;
 
 import edu.bsuir.sneakersshop.domain.entity.enums.SeasonType;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -49,6 +38,22 @@ public class Product implements Serializable {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private SeasonType seasonType;
+
+    @ManyToMany(
+            fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL
+    )
+    @JoinTable(
+            joinColumns = @JoinColumn(
+                    name = "product_id",
+                    referencedColumnName = "id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "category_id",
+                    referencedColumnName = "id"
+            )
+    )
+    private Collection<Category> categories;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Brand brand;

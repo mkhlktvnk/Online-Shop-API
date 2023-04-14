5a621857e47038cc3060dae63d16c441eb8b9efe
misha <mikhail.kutovenko@gmail.com>
2023-04-14 08:42:50 +0300

    Refactor imports

--- src/main/java/edu/bsuir/sneakersshop/domain/repository/ProductRepository.java
+++ src/main/java/edu/bsuir/sneakersshop/domain/repository/ProductRepository.java
@@ -1,16 +1,12 @@
 package edu.bsuir.sneakersshop.domain.repository;
 
 import edu.bsuir.sneakersshop.domain.entity.Product;
-import edu.bsuir.sneakersshop.domain.entity.enums.SeasonType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Long>,

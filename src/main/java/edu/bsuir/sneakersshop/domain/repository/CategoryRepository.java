732feb460745c859bba748d03a629a9bbcde3372
misha <mikhail.kutovenko@gmail.com>
2023-04-14 08:47:45 +0300

    Update CategoryRepository: add specifications executor

--- src/main/java/edu/bsuir/sneakersshop/domain/repository/CategoryRepository.java
+++ src/main/java/edu/bsuir/sneakersshop/domain/repository/CategoryRepository.java
@@ -4,10 +4,11 @@ import edu.bsuir.sneakersshop.domain.entity.Category;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long>, JpaSpecificationExecutor<Category> {
 
     Page<Category> findAllByProductsId(Long productId, Pageable pageable);
 

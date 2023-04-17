bef6654f2029a1dcfbd11d4c16f804a5ad502cab
misha <mikhail.kutovenko@gmail.com>
2023-04-17 21:55:11 +0300

    Update OrderMapper: add links

--- src/main/java/edu/bsuir/onlineshop/web/mapper/OrderMapper.java
+++ src/main/java/edu/bsuir/onlineshop/web/mapper/OrderMapper.java
@@ -1,17 +1,49 @@
 package edu.bsuir.onlineshop.web.mapper;
 
 import edu.bsuir.onlineshop.domain.entity.Order;
+import edu.bsuir.onlineshop.web.controller.OrderController;
+import edu.bsuir.onlineshop.web.controller.ProductController;
+import edu.bsuir.onlineshop.web.controller.UserController;
 import edu.bsuir.onlineshop.web.model.OrderModel;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.springframework.http.HttpMethod;
 
 import java.util.Collection;
 import java.util.List;
 
-@Mapper(uses = ProductMapper.class)
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Mapper(uses = {
+        ProductMapper.class,
+        UserMapper.class
+})
 public interface OrderMapper {
+
     @Mapping(target = "productModel", source = "product")
     OrderModel mapToModel(Order order);
 
     List<OrderModel> mapToModel(Collection<Order> orders);
+
+    @AfterMapping
+    default void addLinks(@MappingTarget OrderModel orderModel) {
+        orderModel.add(
+                linkTo(methodOn(OrderController.class)
+                        .findById(orderModel.getId()))
+                        .withSelfRel()
+                        .withType(HttpMethod.GET.name()),
+                linkTo(methodOn(ProductController.class)
+                        .getProductById(orderModel.getProductModel().getId()))
+                        .withRel("product")
+                        .withType(HttpMethod.GET.name()),
+                linkTo(methodOn(UserController.class)
+                        .findByUsername(orderModel.getUserModel().getUsername()))
+                        .withRel("customer")
+                        .withType(HttpMethod.GET.name())
+        );
+    }
+
 }

e85babbd58cd15667e4d069093af7fe22ab64085
misha <mikhail.kutovenko@gmail.com>
2023-04-17 21:49:22 +0300

    Add UserController

---
+++ src/main/java/edu/bsuir/onlineshop/web/controller/UserController.java
@@ -0,0 +1,37 @@
+package edu.bsuir.onlineshop.web.controller;
+
+import edu.bsuir.onlineshop.domain.entity.User;
+import edu.bsuir.onlineshop.service.UserService;
+import edu.bsuir.onlineshop.web.mapper.UserMapper;
+import edu.bsuir.onlineshop.web.model.UserModel;
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v0")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    private final PagedResourcesAssembler<User> pagedResourcesAssembler;
+    private final UserMapper mapper = Mappers.getMapper(UserMapper.class);
+
+    @GetMapping("/users")
+    public PagedModel<UserModel> findAll(@PageableDefault Pageable pageable) {
+        Page<User> users = userService.findAll(pageable);
+        return pagedResourcesAssembler.toModel(users, mapper::mapToModel);
+    }
+
+    @GetMapping("/users/{username}")
+    public UserModel findByUsername(@PathVariable String username) {
+        return (UserModel) userService.loadUserByUsername(username);
+    }
+}

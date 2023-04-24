61bc2a0ce21781a279c4d1f06e036f28e2ae9347
misha <mikhail.kutovenko@gmail.com>
2023-04-24 13:06:41 +0300

    Add LinkHandler

---
+++ src/main/java/edu/bsuir/onlineshop/web/model/link/handler/LinkHandler.java
@@ -0,0 +1,37 @@
+package edu.bsuir.onlineshop.web.model.link.handler;
+
+import edu.bsuir.onlineshop.domain.entity.User;
+import edu.bsuir.onlineshop.domain.entity.enums.RoleType;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public abstract class LinkHandler<T extends RepresentationModel<T>> {
+    public void addLinks(T model) {
+        addCommonLinks(model);
+        getCurrentUser().ifPresent(user -> {
+            if (user.getAuthorities().stream().anyMatch(authority ->
+                    authority.getAuthority().equals(RoleType.USER.name()))) {
+                addUserRelatedLinks(model);
+            } else if (user.getAuthorities().stream().anyMatch(authority ->
+                    authority.getAuthority().equals(RoleType.ADMIN.name()))) {
+                addAdminRelatedLinks(model);
+            }
+        });
+    }
+
+    private Optional<User> getCurrentUser() {
+        return Optional.of(
+                (User) SecurityContextHolder.getContext()
+                        .getAuthentication()
+                        .getPrincipal()
+        );
+    }
+
+    protected abstract void addCommonLinks(T model);
+
+    protected abstract void addUserRelatedLinks(T model);
+
+    protected abstract void addAdminRelatedLinks(T model);
+}

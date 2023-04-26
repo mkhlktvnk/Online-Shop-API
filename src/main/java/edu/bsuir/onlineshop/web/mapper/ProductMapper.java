79c1ebde025f425aba21400cd0d95c213bc52f3c
misha <mikhail.kutovenko@gmail.com>
2023-04-26 15:47:28 +0300

    Refactor imports

--- src/main/java/edu/bsuir/onlineshop/web/mapper/ProductMapper.java
+++ src/main/java/edu/bsuir/onlineshop/web/mapper/ProductMapper.java
@@ -1,20 +1,10 @@
 package edu.bsuir.onlineshop.web.mapper;
 
 import edu.bsuir.onlineshop.domain.entity.Product;
-import edu.bsuir.onlineshop.web.controller.CategoryController;
-import edu.bsuir.onlineshop.web.controller.OrderController;
-import edu.bsuir.onlineshop.web.controller.ProductController;
-import edu.bsuir.onlineshop.web.controller.ReviewController;
 import edu.bsuir.onlineshop.web.model.ProductModel;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.springframework.http.HttpMethod;
 
 import java.util.Collection;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Mapper(uses = {
         ImageMapper.class,
